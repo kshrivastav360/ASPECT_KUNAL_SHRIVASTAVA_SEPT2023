328c31c98b45dc02e65d7a65357354d9589e4074
kshrivastav360 <145746248+kshrivastav360@users.noreply.github.com>
2023-10-14 18:09:52 +0530

    Add files via upload

---
+++ profit or loss.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int sp,cp;
+	printf("enter cp value :");
+	scanf("%d",&sp);
+		printf("enter sp value :");
+	scanf("%d",&sp);
+	if (sp>cp) {
+		printf("profit");
+	}
+	else {
+		printf("loss");
+	}
+	return 0;
+}
