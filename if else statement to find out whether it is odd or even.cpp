419658291ab5b102e52fe5c3ddceba65d2628090
kshrivastav360 <145746248+kshrivastav360@users.noreply.github.com>
2023-10-14 18:15:59 +0530

    Add files via upload

---
+++ if else statement to find out whether it is odd or even.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("enter n value :");
+	scanf("%d",&n);
+	if (n%2==0){
+		printf("it is an even no");
+	}
+	else {
+		printf("it is an odd no");
+	}
+	return 0;
+}
