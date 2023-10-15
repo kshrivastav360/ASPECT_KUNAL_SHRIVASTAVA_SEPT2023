8bd0fc1b37b27262f8eb96cf5c37f3677df0189b
kshrivastav360 <145746248+kshrivastav360@users.noreply.github.com>
2023-10-15 08:42:32 +0530

    Add files via upload

---
+++ length of string.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+0;
+}	char str[40];
+	int size = 0;
+	int i=0;
+	
+	puts("Enter a string : ");
+	scanf("%[^\n]",str);
+	
+	puts ("your size is :");
+	while(str[i]!='\o'){
+	    size++;
+	    i++;
+	}
+
+	printf("%d ",size);
+	return 0;
+	
+
