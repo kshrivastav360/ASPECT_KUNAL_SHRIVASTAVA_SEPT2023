0150838a8f9a8d465d40683d494218cc3103eda9
kshrivastav360 <145746248+kshrivastav360@users.noreply.github.com>
2023-10-15 08:55:32 +0530

    Add files via upload

---
+++ binary search.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+	int arr[11] = {2,17,23,34,56,61,69,78,84,91,98};
+	int low = 0;
+	int high = 10;
+	int key;
+	
+	printf("Serach the no to search : " );
+	scanf("%d",&key);
+	
+	while (low<=high){
+		int mid=(low+high)/2;
+	       if (key<arr[mid]){
+	       	high=mid-1;
+		   }
+		   else if (key>arr[mid]){
+		   	low=mid+1;
+		   }
+		   else if (key==arr[mid]){
+		   	printf("Found the location : %d\n",mid);
+		   	break;
+		   }
+	}
+	return 0;
+}
