2f613e16b8b1a8e580f3e552337960092d76d6a6
kshrivastav360 <145746248+kshrivastav360@users.noreply.github.com>
2023-10-16 07:53:29 +0530

    Add files via upload

---
+++ factorial with argu and no return.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void fact(int x){
+	
+	printf("Enter the value of n :");
+	scanf("%d",&x);
+	int facto=1;
+	for(int i=2;i<=x;i++){
+	 facto=facto*i;
+	}
+	printf("The factorial is : %d",facto);
+	return ;
+	
+}
+int main(){
+	int y;
+	fact(y);
+
+	
+	return 0;
+}
