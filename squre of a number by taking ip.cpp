dfd15846f5f6b26649024e9bd40807e401ef21d4
kshrivastav360 <145746248+kshrivastav360@users.noreply.github.com>
2023-11-06 16:16:30 +0530

    Add files via upload

---
+++ squre of a number by taking ip.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x;
+	cout<<"enter the value of x :"<<endl;
+	cin>>x;
+	cout<<"square the value of x is :"<<endl;
+	cout<<x*x<<endl;
+	return 0;
+}
